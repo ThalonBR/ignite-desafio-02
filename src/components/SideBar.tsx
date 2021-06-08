aafa8291369cf1574fa2d62df91be7b2229a49e7
Vittor Duarte <vittor_vi@hotmail.com>
2021-06-08 13:42:20 -0300

    feat: create sidebar and content component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+import { GenreResponseProps } from '../App';
+
+
+interface ISidebarProps {
+  handleClickButton(genreId: number): void
+  readonly selectedGenreId: number
+  readonly genres: readonly GenreResponseProps[]
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: ISidebarProps) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
